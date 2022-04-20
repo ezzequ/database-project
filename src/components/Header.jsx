1fe55fc6bb2661f7fcd1e2a4fd2ece0ee1a3f513
elinarnten <elin.arnten@hotmail.com>
2022-04-20 14:21:12 +0200

    removed the problem with CSSProperties

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,24 @@
-import { CSSProperties } from "react";
 import Logo from "../assets/logo.png";
 
 export default function Header() {
   return (
-    <header style={headerContainer}>
-      <img src={Logo} alt="" style={setLogo} />
-      <div style={headerText}>
+    <header
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        height: "7rem",
+      }}
+    >
+      <img
+        src={Logo}
+        alt=""
+        style={{ display: "flex", height: "5rem", margin: "1rem" }}
+      />
+      <div style={{ display: "flex", marginRight: "2rem" }}>
         <p>Log In</p>
-        <p style={space}> | </p>
+        <p style={{ paddingRight: "1rem", paddingLeft: "1rem" }}> | </p>
         <p>Sign Up</p>
       </div>
     </header>
   );
 }
-const headerContainer: CSSProperties = {
-  display: "flex",
-  justifyContent: "space-between",
-  height: "7rem",
-};
-
-const headerText: CSSProperties = {
-  display: "flex",
-  marginRight: "2rem",
-};
-
-const space: CSSProperties = {
-  paddingRight: "1rem",
-  paddingLeft: "1rem",
-};
-const setLogo: CSSProperties = {
-  display: "flex",
-  height: "5rem",
-  margin: "1rem",
-};
