2ce60006a23a611d59956a158d946dee5812791e
ezzequ <esther.reichmann@me.com>
2022-04-20 15:08:47 +0200

    added login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,43 @@
-
+import React from "react";
+import "../style/Login.css"
 
 function Login() {
   return (
     <>
       <div>
-        <p>hej hej logga in här</p>
+        <h1>Welcome To The Book Circle!</h1>
+        <p>
+          Get inspired and browse thousands of book reviews, contribute, add and
+          share your own
+        </p>
       </div>
-      <div>
-        <form>
-          <div>
-            <label for="userName">User Name</label>
-            <input name="userName"></input>
+      <div className="LogInCreateAccountBox">
+      
+          <div class="CreateAccountPlaceHolder">
+            <h2>Create Account</h2>
+            <p></p>
+            <button>Create Account</button>
           </div>
-          <div>
-            <label for="passWord">Password</label>
-            <input name="passWord"></input>
-          </div>
-          <button>log in</button>
-          <button>create account</button>
-        </form>
+
+          <div className="loginPlaceHolder">
+            <h2>Login or Continue Offline</h2>
+            <form>
+              <div className="InputHolder">
+                <label for="userName">Username</label>
+                <input name="userName"></input>
+              </div>
+              <div className="InputHolder">
+                <label for="passWord">Password</label>
+                <input name="passWord"></input>
+              </div>
+              <div className="logInButtonPlaceHolder">
+                <p>
+                  Forgot Login details?<p style={{color:"red"}}>click here</p>
+                </p>
+                <button>Log in</button>
+              </div>
+            </form>
+            </div>
       </div>
     </>
   );
